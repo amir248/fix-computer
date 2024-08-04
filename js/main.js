a60071b16525296a684812d58f5d8983b4bb1cc4
chikchilcy <chikchicly@gmail.com>
2024-08-04 18:52:48 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// window.addEventListener('load',includeScript);
+setTimeout(()=>{
+    let script = document.createElement('script');
+    script.src = "js/include.js";
+    document.body.append(script);
+},700);
