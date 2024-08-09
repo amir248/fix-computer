a9688e6388934e434ee50a050c9041670a1c8733
chikchilcy <chikchicly@gmail.com>
2024-08-09 19:03:51 +0500

    async await

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
-// window.addEventListener('load',includeScript);
-// setTimeout(()=>{
-    let script = document.createElement('script');
-    script.src = "js/include.js";
-    document.body.append(script);
-// },700);
-setTimeout(()=>{
-    let menu = document.createElement('script');
-    menu.src = "js/menu.js";
-    document.body.append(menu);
-},700)
+function headerOnload(){
+    return new Promise((resolve)=>{
+        function first(){
+            let script = document.createElement('script');
+            script.src = "js/include.js";
+            document.body.append(script);
+        };
+        resolve(first());
+    });
+}
+
+function bodyDocument(){
+    return new Promise((resolve)=>{
+        function itsBody(){
+            setTimeout(()=>{
+                let menu = document.createElement('script');
+                menu.src = "js/menu.js";
+                document.body.append(menu);
+            },1700);
+        };
+        resolve(itsBody());
+    })
+}
+
+async function loadNewScript(){
+    await headerOnload();
+    await bodyDocument();
+};
+window.addEventListener("DOMContentLoaded",loadNewScript);
